efdcd8a58f1edc3fe30f244a350ebb30f70e470b
James Phillips <jamesdphillips@gmail.com>
2019-04-05 15:05:25 -0700

    Upload package to S3
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

---
+++ scripts/upload-artifacts.js
@@ -0,0 +1,40 @@
+import AWS from "aws-sdk";
+import fs from "fs";
+import path from "path";
+import chalk from "chalk";
+import git from "git-rev-sync";
+
+const currentRev = git.long();
+const pkgFilename = "dashboard.tgz";
+
+const bucket = "sensu-ci-web-build";
+const pathPrefix = "/oss/webapp";
+const outPath = path.join(pathPrefix, currentRev, "web.tgz");
+
+// Setup client
+const s3 = new AWS.S3();
+
+// Read file from disk
+let pkgFile;
+try {
+  const pkgPath = path.join(__dirname, "..", pkgFilename);
+  console.info(pkgPath);
+  pkgFile = fs.readFileSync(pkgPath);
+} catch (e) {
+  console.error(
+    chalk.red("error"),
+    "unable to find package file; ensure that you have built it before attempting to upload",
+    e,
+  );
+  process.exit(1);
+}
+
+// Upload
+console.info(chalk.red("info"), "uploading artifact to", outPath);
+s3.upload({ Bucket: bucket, Key: outPath, Body: pkgFile }, err => {
+  if (err) {
+    console.error(chalk.red("error"), err);
+    process.exit(1);
+  }
+  console.info(chalk.red("info"), "successfully uploaded files");
+});
