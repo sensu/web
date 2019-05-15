caf72e5aa42eafa3f2676acfba1a8c4c72beec40
Wesley Yu <wesleyu89@gmail.com>
2019-05-15 14:23:02 -0700

    Create HandlerView component to fetch and render a collection of handlers
    The HandlerView component will be used to render the view that displays a list of handlers. The HandlerView provides the application layout and fetches handlers by constructing the GraphQL query.
    
    Signed-off-by: Wesley Yu <wesleyu89@gmail.com>

--- src/lib/component/view/index.js
+++ src/lib/component/view/index.js
@@ -4,6 +4,7 @@ export { default as EntitiesView } from "./EntitiesView";
 export { default as EntityDetailsView } from "./EntityDetailsView";
 export { default as EventDetailsView } from "./EventDetailsView";
 export { default as EventsView } from "./EventsView";
+export { default as HandlersView } from "./HandlersView";
 export { default as NamespaceNotFoundView } from "./NamespaceNotFoundView";
 export { default as NotFoundView } from "./NotFoundView";
 export { default as SignInView } from "./SignInView";
