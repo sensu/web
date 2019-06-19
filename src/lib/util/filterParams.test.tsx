fad17cc096e527575f66ae8982b6e963cc993f07
James Phillips <jamesdphillips@gmail.com>
2019-06-19 12:54:15 -0700

    Add tests for filter params module
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

---
+++ src/lib/util/filterParams.test.tsx
@@ -0,0 +1,28 @@
+import { buildFilter, buildFilterParams, parseFilter, parseFilterParams, toggleParam } from "./filterParams";
+
+describe("filter param utils", () => {
+  test("parseFilter", () => {
+    expect(parseFilter("")).toEqual(["",""]);
+    expect(parseFilter("foobar")).toEqual(["foobar", ""]);
+    expect(parseFilter("foo:bar")).toEqual(["foo", "bar"]);
+    expect(parseFilter("foo:bar:baz")).toEqual(["foo", "bar:baz"]);
+  });
+  test("parseFilterParams", () => {
+    expect(parseFilterParams("foo:bar")).toEqual({foo: "bar"});
+    expect(parseFilterParams(["foo:bar"])).toEqual({foo: "bar"});
+    expect(parseFilterParams(["foo:bar", "bar:baz"])).toEqual({foo: "bar", bar: "baz"});
+  });
+  test("buildFilter", () => {
+    expect(buildFilter("foo", "bar")).toEqual("foo:bar");
+    expect(buildFilter("foo", "bar:baz")).toEqual("foo:bar:baz");
+  });
+  test("buildFilterParams", () => {
+    expect(buildFilterParams({foo:"bar"})).toEqual(["foo:bar"]);
+    expect(buildFilterParams({foo:"bar:baz"})).toEqual(["foo:bar:baz"]);
+  });
+  test("toggleParam", () => {
+    let x;
+    toggleParam("foo", fn => { x = fn({bar:"baz"}) })("bar");
+    expect(x).toEqual({foo: "bar", bar: "baz"});
+  });
+});
