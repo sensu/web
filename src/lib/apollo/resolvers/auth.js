6c35f27d790227253a97b08972c6962fb5c3aae5
James Phillips <jamesdphillips@gmail.com>
2019-11-28 16:39:37 -0800

    omit access token when the token is invalid or expired
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- src/lib/apollo/resolvers/auth.js
+++ src/lib/apollo/resolvers/auth.js
@@ -11,6 +11,7 @@ import { UnauthorizedError } from "/lib/error/FetchError";
 const query = gql`
   query AuthQuery {
     auth @client {
+      invalid
       accessToken
       refreshToken
       expiresAt
@@ -79,6 +80,16 @@ export default {
           throw new TypeError(
             "invalid `notBefore` variable. Expected DateTime",
           );
+        }
+
+        if (result.auth.invalid || !result.auth.refreshToken) {
+          return {
+            __typename: "RefreshTokensMutation",
+            auth: {
+              ...result,
+              accessToken: null,
+            },
+          };
         }
 
         const expired =
