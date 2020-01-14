87df8783adc699ef5c06b6bbbd0fbe677af4ff24
Melissa Page <73hl10n@gmail.com>
2020-01-14 15:13:39 -0800

    Add image label support
    Signed-off-by: Melissa Page <73hl10n@gmail.com>

--- src/lib/component/partial/Label/Label.js
+++ src/lib/component/partial/Label/Label.js
@@ -16,6 +16,7 @@ const styles = theme => ({
     paddingLeft: theme.spacing(1),
     paddingRight: theme.spacing(1),
     borderRadius: `${theme.spacing(0.5)}px 0 0 ${theme.spacing(0.5)}px`,
+    border: `1px solid ${theme.palette.primary.main}`,
   },
   value: {
     color: theme.palette.text.primary,
@@ -23,16 +24,23 @@ const styles = theme => ({
     paddingLeft: theme.spacing(1),
     paddingRight: theme.spacing(1),
     borderRadius: `0 ${theme.spacing(0.5)}px ${theme.spacing(0.5)}px 0`,
+    border: `1px solid ${emphasize(theme.palette.primary.main, 0.7)}`,
+  },
+  imgContainer: {
+    maxWidth: "100%",
+    width: "fit-content",
   },
   fwKey: {
     color: theme.palette.getContrastText(theme.palette.primary.main),
     background: theme.palette.primary.main,
+    border: `1px solid ${theme.palette.primary.main}`,
     textAlign: "center",
     borderRadius: `${theme.spacing(0.5)}px ${theme.spacing(0.5)}px 0 0`,
   },
   image: {
-    borderRadius: `0 0 ${theme.spacing(0.5)} ${theme.spacing(0.5)}`,
-    display: "block",
+    border: `1px solid ${emphasize(theme.palette.primary.main, 0.7)}`,
+    borderRadius: `0 0 ${theme.spacing(0.5)}px ${theme.spacing(0.5)}px`,
+    maxWidth: "100%",
   },
   icon: {
     paddingLeft: theme.spacing(0.3),
@@ -40,6 +48,21 @@ const styles = theme => ({
     fontSize: "18px",
   },
 });
+
+const imageExtensions = [
+  ".PNG",
+  ".JPG",
+  ".JPEG",
+  ".GIF",
+  ".WEBP",
+  ".TIFF",
+  ".PSD",
+  ".RAW",
+  ".BMP",
+  ".HEIF",
+  ".INDD",
+  ".SVG.",
+];
 
 class Label extends React.PureComponent {
   static propTypes = {
@@ -51,48 +74,63 @@ class Label extends React.PureComponent {
   parseLink = (value, name) => {
     try {
       new URL(value);
-      // TODO make sure to have a list of all different image file types,
-      // then do the fetch request after to be sure
-      if (value.includes("jpg")) {
+      // see if it's got an image extension
+      let results = imageExtensions.filter(ext =>
+        value.toUpperCase().includes(ext),
+      );
+      let imageMatch = results.length > 0;
+      // if that fails, we should check the content type in case
+      if (!imageMatch) {
+        fetch(value, {
+          method: "HEAD",
+        })
+          .then(response => response.headers.get("Content-type"))
+          .then(type => {
+            imageMatch = type === "image/jpeg" ? true : false;
+          });
+      }
+      // return an image
+      if (imageMatch) {
         return (
-          <div>
+          <div className={this.props.classes.imgContainer}>
             <div className={this.props.classes.fwKey}>{name}</div>
             <div>
               <img
                 className={this.props.classes.image}
                 src={value}
-                alt={"bunny"}
+                alt={value}
               />
             </div>
           </div>
         );
       }
-      // fetch(value).then(response => console.log(response));
-    } catch (e) {
+      // return a link
       return (
-        <div>
+        <span>
           <span className={this.props.classes.key}>{name}</span>
-          <span className={this.props.classes.value}>{value}</span>;
-        </div>
+          <span className={this.props.classes.value}>
+            <Link href={value}>
+              {value}
+              <LinkIcon className={this.props.classes.icon} />
+            </Link>
+          </span>
+        </span>
+      );
+    } catch (e) {
+      // catch if it's not a url, it's a regular value
+      return (
+        <span>
+          <span className={this.props.classes.key}>{name}</span>
+          <span className={this.props.classes.value}>{value}</span>
+        </span>
       );
     }
-    return (
-      <div>
-        <span className={this.props.classes.key}>{name}</span>
-        <span className={this.props.classes.value}>
-          <Link href={value}>
-            {value}
-            <LinkIcon className={this.props.classes.icon} />
-          </Link>
-        </span>
-      </div>
-    );
   };
 
   render() {
     const { classes, name, value } = this.props;
     return (
-      <Typography component="span" className={classes.root}>
+      <Typography component="div" className={classes.root}>
         {this.parseLink(value, name)}
       </Typography>
     );
