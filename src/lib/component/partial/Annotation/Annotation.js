c95b2a0ae899f5751b02f1a2f61829245cd1c076
Melissa Page <73hl10n@gmail.com>
2020-02-19 11:35:25 -0800

    Wrap codeblock
    Signed-off-by: Melissa Page <73hl10n@gmail.com>

--- src/lib/component/partial/Annotation/Annotation.js
+++ src/lib/component/partial/Annotation/Annotation.js
@@ -1,70 +1,16 @@
 import React from "/vendor/react";
 import PropTypes from "prop-types";
-import classnames from "/vendor/classnames";
-import { withStyles, Typography } from "/vendor/@material-ui/core";
-import { emphasize } from "/vendor/@material-ui/core/styles/colorManipulator";
+import { CodeBlock } from "/lib/component/base/CodeBlock";
 import { parseLink } from "/lib/component/util";
-
-const styles = theme => ({
-  root: {
-    fontFamily: theme.typography.monospace.fontFamily,
-    overflowX: "auto",
-    userSelect: "text",
-    tabSize: 2,
-    color:
-      theme.palette.type === "dark"
-        ? theme.palette.secondary.light
-        : theme.palette.secondary.dark,
-  },
-  background: {
-    backgroundColor: emphasize(theme.palette.background.paper, 0.01875),
-  },
-  scaleFont: {
-    // Browsers tend to render monospaced fonts a little larger than intended.
-    // Attempt to scale accordingly.
-    fontSize: "0.8125rem", // TODO: Scale given fontSize from theme?
-  },
-  wrap: { whiteSpace: "pre-wrap" },
-});
 
 class Annotation extends React.Component {
   static propTypes = {
-    background: PropTypes.bool,
-    classes: PropTypes.object.isRequired,
-    className: PropTypes.string,
-    component: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
     children: PropTypes.node.isRequired,
-    scaleFont: PropTypes.bool,
-  };
-
-  static defaultProps = {
-    background: true,
-    component: "pre",
-    className: "",
-    scaleFont: true,
   };
 
   render() {
-    const {
-      background,
-      classes,
-      className: classNameProp,
-      children,
-      scaleFont,
-      ...props
-    } = this.props;
-
-    const className = classnames(classes.root, classNameProp, {
-      [classes.background]: background,
-      [classes.scaleFont]: scaleFont,
-    });
-
-    return (
-      <Typography className={className} {...props}>
-        <code className={classes.wrap}>{parseLink(children)}</code>
-      </Typography>
-    );
+    return <CodeBlock>{parseLink(this.props.children)}</CodeBlock>;
   }
 }
 
-export default withStyles(styles)(Annotation);
+export default Annotation;
