b07789e2a294db274dd09492c1d0dfe29296b404
James Phillips <jamesdphillips@gmail.com>
2020-02-28 15:55:43 -0800

    fix overlapping switch headings in FF
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- src/lib/component/partial/ContextSwitcher/ContextSwitcherListHeader.tsx
+++ src/lib/component/partial/ContextSwitcher/ContextSwitcherListHeader.tsx
@@ -3,9 +3,12 @@ import { fade } from "/vendor/@material-ui/core/styles/colorManipulator";
 
 const ContextSwitcherListHeader = styled(ListSubheader)<Theme>(
   ({ theme }) => ({
-    backgroundColor: fade(theme.palette.background.paper, 0.5),
-    backdropFilter: "blur(8px)",
+    backgroundColor: theme.palette.background.paper,
     lineHeight: `${theme.spacing(4)}px`,
+    "@supports (backdrop-filter: blur(4px)) or (-webkit-backdrop-filter: blur(4px))": {
+      backgroundColor: fade(theme.palette.background.paper, 0.5),
+      backdropFilter: `blur(${theme.spacing(1)}px)`,
+    },
   }),
   { name: "ContextSwitcherListHeader" },
 );
