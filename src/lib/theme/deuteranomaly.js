7eeea58e8dc16d1edeade7cc7c15833f71f56268
Melissa Page <73hl10n@gmail.com>
2020-03-17 16:50:23 -0700

    darken grey
    Signed-off-by: Melissa Page <73hl10n@gmail.com>

--- src/lib/theme/deuteranomaly.js
+++ src/lib/theme/deuteranomaly.js
@@ -9,7 +9,7 @@ const theme = (type = "light") =>
       critical: { main: colors.lime[500] },
       warning: { main: colors.lime[900], secondary: colors.lime[900] },
       unknown: { main: colors.indigo[300] },
-      success: { main: colors.grey[300], secondary: colors.grey[400] },
+      success: { main: colors.grey[400], secondary: colors.grey[400] },
 
       primary: {
         light: colors.indigo[400],
