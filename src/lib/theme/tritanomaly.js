39efca1b00b1bf97a48ef368c1701de56e95450b
Melissa Page <73hl10n@gmail.com>
2020-03-19 13:51:37 -0700

    update tri
    Signed-off-by: Melissa Page <73hl10n@gmail.com>

--- src/lib/theme/tritanomaly.js
+++ src/lib/theme/tritanomaly.js
@@ -8,26 +8,26 @@ const theme = (type = "light") =>
 
       critical: {
         light: colors.teal[200],
-        main: colors.red[500],
-        dark: colors.red[700],
+        main: colors.teal[500],
+        dark: colors.teal[700],
         contrastText: "#F3F5F7",
       },
       error: {
-        light: colors.teal[200],
-        main: colors.pink[400],
-        dark: colors.pink[700],
-        contrastText: "#F3F5F7",
-      },
-      warning: {
         light: colors.teal[200],
         main: colors.teal[500],
         dark: colors.teal[700],
         contrastText: "#F3F5F7",
       },
-      unknown: {
+      warning: {
         light: colors.blue[50],
         main: colors.blue[300],
         dark: colors.blue[500],
+        contrastText: "#F3F5F7",
+      },
+      unknown: {
+        light: colors.red[200],
+        main: colors.red[700],
+        dark: colors.red[700],
         contrastText: "#F3F5F7",
       },
       success: {
