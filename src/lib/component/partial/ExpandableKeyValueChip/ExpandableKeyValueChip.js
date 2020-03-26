sensu/web

commit 5855da1eded2e4b55f06be48a47635610ab6bdee

fix image label error (#278)
Signed-off-by: Melissa Page <73hl10n@gmail.com>

src/lib/component/partial/KeyValueChip/KeyValueChip.tsx
@@ -1,0 +1,156 @@
+import React from "/vendor/react";
+import {
+  createStyles,
+  makeStyles,
+  Typography,
+  Theme,
+} from "/vendor/@material-ui/core";
+import {
+  darken,
+  emphasize,
+} from "/vendor/@material-ui/core/styles/colorManipulator";
+import classNames from "/vendor/classnames";
+import { AutoLink } from "/lib/component/util";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      color: emphasize(theme.palette.text.primary, 0.22),
+      display: "inline",
+      whiteSpace: "nowrap",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+    },
+    base: {
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+      display: "inline-block",
+      whiteSpace: "nowrap",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      height: "22px",
+    },
+    key: {
+      color: theme.palette.getContrastText(theme.palette.primary.main),
+      background: theme.palette.primary.main,
+      borderRadius: `${theme.spacing(0.5)}px 0 0 ${theme.spacing(0.5)}px`,
+      border: `1px solid ${theme.palette.primary.main}`,
+      maxWidth: "160px",
+    },
+    singleKey: {
+      borderRadius: `${theme.spacing(0.5)}px`,
+    },
+    value: {
+      color: darken(theme.palette.primary.main, 0.7),
+      background: emphasize(theme.palette.primary.main, 0.7),
+      borderRadius: `0 ${theme.spacing(0.5)}px ${theme.spacing(0.5)}px 0`,
+      border: `1px solid ${emphasize(theme.palette.primary.main, 0.7)}`,
+      maxWidth: "200px",
+    },
+    imageContainer: {
+      maxWidth: "100%",
+      width: "fit-content",
+    },
+    imageKey: {
+      color: theme.palette.getContrastText(theme.palette.primary.main),
+      background: theme.palette.primary.main,
+      border: `1px solid ${theme.palette.primary.main}`,
+      textAlign: "center",
+      borderRadius: `${theme.spacing(0.5)}px ${theme.spacing(0.5)}px 0 0`,
+      width: "100%",
+      display: "block",
+    },
+    image: {
+      border: `1px solid ${emphasize(theme.palette.primary.main, 0.7)}`,
+      borderRadius: `0 0 ${theme.spacing(0.5)}px ${theme.spacing(0.5)}px`,
+      maxWidth: "100%",
+    },
+  }),
+);
+
+const imageExtensions = [
+  ".PNG",
+  ".JPG",
+  ".JPEG",
+  ".GIF",
+  ".WEBP",
+  ".TIFF",
+  ".PSD",
+  ".RAW",
+  ".BMP",
+  ".HEIF",
+  ".INDD",
+  ".SVG",
+];
+
+interface KeyProps extends React.ComponentProps<"span"> {
+  name: string;
+  value?: string;
+}
+
+const stringContainer = (
+  classes: any,
+  name: string,
+  value: string,
+  ...props: any
+) => {
+  return (
+    <Typography component="span" className={classes.root} variant="body2">
+      <span
+        className={classNames(
+          classes.base,
+          classes.key,
+          value ? null : classes.singleKey,
+        )}
+        {...props}
+      >
+        {name}
+      </span>
+      {value && (
+        <span className={classNames(classes.base, classes.value)}>
+          <AutoLink value={value} {...props} />{" "}
+        </span>
+      )}
+    </Typography>
+  );
+};
+
+const imageContainer = (
+  classes: any,
+  name: string,
+  value: string,
+  ...props: any
+) => {
+  return (
+    <Typography component="div" className={classes.root} variant="body2">
+      <div className={classes.imageContainer}>
+        <div className={classNames(classes.base, classes.imageKey)} {...props}>
+          {name}
+        </div>
+        <div {...props}>
+          <img className={classes.image} src={value} alt={value} />
+        </div>
+      </div>
+    </Typography>
+  );
+};
+
+const KeyValueChip = ({ name, value = "", ...props }: KeyProps) => {
+  const classes = useStyles();
+
+  let urlpath = "";
+  try {
+    const url = new URL(value);
+    urlpath = url.pathname;
+  } catch (e) {
+    // no url
+  }
+
+  if (imageExtensions.some((ext) => urlpath.toUpperCase().endsWith(ext))) {
+    return imageContainer(classes, name, value, props);
+  }
+
+  return stringContainer(classes, name, value, props);
+};
+
+export default KeyValueChip;

src/lib/component/partial/KeyValueChip/KeyValueChip.js
@@ -1,170 +1,0 @@
-import React from "/vendor/react";
-import PropTypes from "prop-types";
-import { withStyles, Typography } from "/vendor/@material-ui/core";
-import {
-  darken,
-  emphasize,
-} from "/vendor/@material-ui/core/styles/colorManipulator";
-import classNames from "/vendor/classnames";
-import { AutoLink } from "/lib/component/util";
-
-const styles = theme => ({
-  root: {
-    color: emphasize(theme.palette.text.primary, 0.22),
-    display: "inline",
-    whiteSpace: "nowrap",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-  },
-  base: {
-    paddingLeft: theme.spacing(1),
-    paddingRight: theme.spacing(1),
-    display: "inline-block",
-    whiteSpace: "nowrap",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    height: "22px",
-  },
-  key: {
-    color: theme.palette.getContrastText(theme.palette.primary.main),
-    background: theme.palette.primary.main,
-    borderRadius: `${theme.spacing(0.5)}px 0 0 ${theme.spacing(0.5)}px`,
-    border: `1px solid ${theme.palette.primary.main}`,
-    maxWidth: "160px",
-  },
-  singleKey: {
-    borderRadius: `${theme.spacing(0.5)}px`,
-  },
-  value: {
-    color: darken(theme.palette.primary.main, 0.7),
-    background: emphasize(theme.palette.primary.main, 0.7),
-    borderRadius: `0 ${theme.spacing(0.5)}px ${theme.spacing(0.5)}px 0`,
-    border: `1px solid ${emphasize(theme.palette.primary.main, 0.7)}`,
-    maxWidth: "200px",
-  },
-  imageContainer: {
-    maxWidth: "100%",
-    width: "fit-content",
-  },
-  imageKey: {
-    color: theme.palette.getContrastText(theme.palette.primary.main),
-    background: theme.palette.primary.main,
-    border: `1px solid ${theme.palette.primary.main}`,
-    textAlign: "center",
-    borderRadius: `${theme.spacing(0.5)}px ${theme.spacing(0.5)}px 0 0`,
-    width: "100%",
-    display: "block",
-  },
-  image: {
-    border: `1px solid ${emphasize(theme.palette.primary.main, 0.7)}`,
-    borderRadius: `0 0 ${theme.spacing(0.5)}px ${theme.spacing(0.5)}px`,
-    maxWidth: "100%",
-  },
-});
-
-const imageExtensions = [
-  ".PNG",
-  ".JPG",
-  ".JPEG",
-  ".GIF",
-  ".WEBP",
-  ".TIFF",
-  ".PSD",
-  ".RAW",
-  ".BMP",
-  ".HEIF",
-  ".INDD",
-  ".SVG",
-];
-
-class KeyValueChip extends React.PureComponent {
-  static propTypes = {
-    classes: PropTypes.object.isRequired,
-    name: PropTypes.string.isRequired,
-    value: PropTypes.string,
-  };
-
-  static defaultProps = {
-    value: null,
-  };
-
-  imageOrStringContainer = () => {
-    const { value } = this.props;
-    try {
-      const url = new URL(value);
-      // see if it's got an image extension
-      let results = imageExtensions.some(ext =>
-        url.pathname.toUpperCase().endsWith(ext),
-      );
-      // if that fails, we should check the content type in case
-      if (!results) {
-        fetch(value, {
-          method: "HEAD",
-        })
-          .then(response => response.headers.get("Content-type"))
-          .then(type => {
-            results = type === /^image\/.+/.test(type) ? true : false;
-          });
-      }
-      // return an image
-      if (results) {
-        return this.imageContainer();
-      }
-      // return a link
-      return this.stringContainer();
-    } catch (e) {
-      // catch if it's not a url, it's a regular value
-      // We use the same method either way though
-      return this.stringContainer();
-    }
-  };
-
-  stringContainer = () => {
-    const { classes, name, value, ...props } = this.props;
-    return (
-      <Typography component="span" className={classes.root} variant="body2">
-        <span
-          className={classNames(
-            classes.base,
-            classes.key,
-            value ? null : classes.singleKey,
-          )}
-          {...props}
-        >
-          {name}
-        </span>
-        {value && (
-          <span className={classNames(classes.base, classes.value)}>
-            <AutoLink value={value} {...props} />{" "}
-          </span>
-        )}
-      </Typography>
-    );
-  };
-
-  imageContainer = () => {
-    const { classes, name, value, ...props } = this.props;
-    return (
-      <Typography component="div" className={classes.root} variant="body2">
-        <div className={classes.imageContainer}>
-          <div
-            className={classNames(classes.base, classes.imageKey)}
-            {...props}
-          >
-            {name}
-          </div>
-          <div {...props}>
-            <img className={this.props.classes.image} src={value} alt={value} />
-          </div>
-        </div>
-      </Typography>
-    );
-  };
-
-  render() {
-    const { classes, name, value, ...props } = this.props;
-    return this.imageOrStringContainer(name, value, classes, props);
-  }
-}
-
-export default withStyles(styles)(KeyValueChip);

commit c450cebd80467448a951e49e387a75c6dd3a0852

Ensures labels and annotations are clickable (#280)
A regression introduced in #278 meant that the onClick handler was no longer being passed to the chip.

Signed-off-by: James Phillips <jamesdphillips@gmail.com>

src/lib/component/partial/ExpandableKeyValueChip/ExpandableKeyValueChip.js
@@ -40,7 +40,7 @@ class ExpandableKeyValueChip extends React.Component {
         <React.Fragment key={key}>
           <ModalController
             renderModal={({ close }) => (
-              <Dialog open maxWidth TransitionComponent={Slide} onClose={close}>
+              <Dialog open maxWidth="sm" fullWidth TransitionComponent={Slide} onClose={close}>
                 <DialogTitle>{key}</DialogTitle>
                 <DialogContent>
                   <CodeBlock>
@@ -52,7 +52,7 @@ class ExpandableKeyValueChip extends React.Component {
                   </CodeBlock>
                 </DialogContent>
                 <DialogActions>
-                  <Button onClick={close} color="contrast">
+                  <Button onClick={close}>
                     Close
                   </Button>
                 </DialogActions>

src/lib/component/partial/KeyValueChip/KeyValueChip.tsx
@@ -83,17 +83,41 @@ const imageExtensions = [
   ".SVG",
 ];
 
-interface KeyProps extends React.ComponentProps<"span"> {
+interface KeyProps {
   name: string;
   value?: string;
+  onClick?: () => void;
 }
 
-const stringContainer = (
-  classes: any,
-  name: string,
-  value: string,
-  ...props: any
-) => {
+const KeyValueChip = ({ name, value = "", ...props }: KeyProps) => {
+  const classes = useStyles();
+
+  let urlpath = "";
+  try {
+    const url = new URL(value);
+    urlpath = url.pathname;
+  } catch (e) {
+    // no url
+  }
+
+  if (imageExtensions.some((ext) => urlpath.toUpperCase().endsWith(ext))) {
+    return (
+      <Typography component="div" className={classes.root} variant="body2">
+        <div className={classes.imageContainer}>
+          <div
+            className={classNames(classes.base, classes.imageKey)}
+            {...props}
+          >
+            {name}
+          </div>
+          <div {...props}>
+            <img className={classes.image} src={value} alt={value} />
+          </div>
+        </div>
+      </Typography>
+    );
+  }
+
   return (
     <Typography component="span" className={classes.root} variant="body2">
       <span
@@ -113,44 +137,6 @@ const stringContainer = (
       )}
     </Typography>
   );
-};
-
-const imageContainer = (
-  classes: any,
-  name: string,
-  value: string,
-  ...props: any
-) => {
-  return (
-    <Typography component="div" className={classes.root} variant="body2">
-      <div className={classes.imageContainer}>
-        <div className={classNames(classes.base, classes.imageKey)} {...props}>
-          {name}
-        </div>
-        <div {...props}>
-          <img className={classes.image} src={value} alt={value} />
-        </div>
-      </div>
-    </Typography>
-  );
-};
-
-const KeyValueChip = ({ name, value = "", ...props }: KeyProps) => {
-  const classes = useStyles();
-
-  let urlpath = "";
-  try {
-    const url = new URL(value);
-    urlpath = url.pathname;
-  } catch (e) {
-    // no url
-  }
-
-  if (imageExtensions.some((ext) => urlpath.toUpperCase().endsWith(ext))) {
-    return imageContainer(classes, name, value, props);
-  }
-
-  return stringContainer(classes, name, value, props);
 };
 
 export default KeyValueChip;