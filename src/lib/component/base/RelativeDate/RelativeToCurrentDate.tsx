b4b9b18a056b504e65efb90450a7a557ce7870da
James Phillips <jamesdphillips@gmail.com>
2020-05-04 09:56:19 -0700

    Reflect platform & local service info on entity page (#295)
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- src/lib/component/base/RelativeDate/RelativeToCurrentDate.tsx
+++ src/lib/component/base/RelativeDate/RelativeToCurrentDate.tsx
@@ -3,14 +3,12 @@ import { useCurrentDate } from "/lib/component/util";
 
 import RelativeDate from "./RelativeDate";
 
-interface RelativeToCurrentDateProps {
-  refreshInterval: number;
+interface Props {
+  refreshInterval?: number;
+  dateTime: string;
 }
 
-const RelativeToCurrentDate = ({
-  refreshInterval,
-  ...props
-}: RelativeToCurrentDateProps) => {
+const RelativeToCurrentDate = ({ refreshInterval, ...props }: Props) => {
   const now = useCurrentDate(refreshInterval || 1000);
   return <RelativeDate to={now} {...props} />;
 };
