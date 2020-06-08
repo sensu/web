ae53d6bd693dfac60e1ef4a56129a96785e5dfb6
James Phillips <jamesdphillips@gmail.com>
2020-06-08 14:48:49 -0700

    recover config
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

---
+++ config/app.webpack.config.js
@@ -0,0 +1,46 @@
+/* eslint-disable import/no-dynamic-require */
+import fs from "fs";
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import CleanPlugin from "clean-webpack-plugin";
+
+import makeConfig from "./base.webpack.config";
+
+const root = fs.realpathSync(process.cwd());
+const outputPath = path.join(root, "build/app");
+
+export default makeConfig({
+  name: "app",
+
+  entry: {
+    app: [path.join(root, "src/app")],
+  },
+
+  output: {
+    path: path.join(outputPath, "public"),
+    publicPath: "/",
+    devtoolNamespace: "app",
+  },
+
+  plugins: [
+    new CleanPlugin(outputPath, { root }),
+
+    // Generates an `index.html` file with the <script> injected.
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(root, "src/lib/static/index.html"),
+      minify: process.env.NODE_ENV !== "development" && {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+  ],
+});
