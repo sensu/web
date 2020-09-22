c0890b2dad7b2599bc656460edf89c2596d74fb4
James Phillips <jamesdphillips@gmail.com>
2020-09-22 11:54:12 -0700

    Pull relevant bugfixes from upstream project (#340)
    * reset offset when page size is changed
    * expose access token even when refresh token is not present
    
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

---
+++ src/lib/component/partial/Pagination.stories.js
@@ -0,0 +1,21 @@
+import React from "react";
+import withTheme from "/lib/storybook/withTheme";
+
+import Pagination from "./Pagination";
+
+export default {
+  title: "partial / Pagination",
+  component: Pagination,
+  decorators: [withTheme],
+};
+
+export const withControls = (args) => (
+  <Pagination {...args} onChangeQuery={() => null} />
+);
+withControls.args = {
+  pageInfo: {
+    totalCount: 200,
+  },
+  offset: 0,
+  limit: 25,
+};
