fb5e90484b60da84ba8311dea4e97335f15a07eb
James Phillips <jamesdphillips@gmail.com>
2021-11-08 15:39:20 -0800

    fixup jest
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- config/jest/cssTransform.js
+++ config/jest/cssTransform.js
@@ -1,10 +1,12 @@
 // This is a custom Jest transformer turning style imports into empty objects.
 // http://facebook.github.io/jest/docs/tutorial-webpack.html
 
-export function process() {
-  return "module.exports = {};";
-}
-export function getCacheKey() {
-  // The output is always the same.
-  return "cssTransform";
-}
+module.exports = {
+  process() {
+    return "module.exports = {};";
+  },
+  getCacheKey() {
+    // The output is always the same.
+    return "cssTransform";
+  },
+};
